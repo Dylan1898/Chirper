77b2f2288c33c32c332e86c802881e13cbd8726e
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-07-19 17:07:03 -0500

    get and post done

--- client/app/app.js
+++ client/app/app.js
@@ -1,85 +1,62 @@
-var myApp = angular.module("myApp", ["ngRoute" ]);
 
-myApp.config(function($routeProvider) {
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config(function ($routeProvider, $locationProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "views/home.html"
-    })
-    .when("/list", {
-        templateUrl : "views/list.html",
-        // controller: 'RouteController'
-    })
-    .when("/single", {
-        templateUrl : "views/single.html"
-    })
-});
-//   myApp.controller("RouteController", function($scope) {
-
-//     })
-
-
-myApp.controller('allGetController', function($scope, $http) {
-  $http.get("http://localhost:3000/api/chirps")
-  .then(function(response) {
-      $scope.allChirps = response.data;
-  })
-
-
-
-$scope.add= function(clicker){
-    $http.post('http://localhost:3000/api/chirps',{text:$scope.new.chirp.user}) .sucess(function(data){
-        $scope.chirp.user=data;
-        $scope.new.chirp.user=''
-        console.log('yeh')
-    })
-}
-// myApp.controller('postController', function clicker($scope, $http) {
-//     console.log('in')
-//     $scope.chirp;
-//     $scope.submit.Form=function(){
-//     // $scope.postFunc= function(){
-//     //     console.log('clicked')
-// $http({
-//                 method: "POST",
-//                 url: "http://localhost:3000/api/chirps",
-// 				data: $scope.chirp,
-//                 datatype: "json",
-//             }).then(function mySucces(response) {
-//                console.log(response);
-//             }, function myError(response) {
-//                  console.log(response);
-//             }); 
-
-
-//     }
-
-
-
-
-
-
-
-
-    //   $http.post("http://localhost:3000/api/chirps")
-//   .then(function(response) {
-      
-//       console.log(response.data)
-//       $scope.allChirps = response.data;
-//         })
+        .when("/", {
+            templateUrl: "views/home.html"
+        })
+        .when("/list", {
+            templateUrl: "views/list.html",
+            // controller: 'RouteController'
+        })
+        .when("/single/:id", {
+            templateUrl: "views/single.html"
+        })
+    // $locationProvider.html5Mode(true);
 });
 
-// angular.module('postController', [])
-// .controller('myCtrl', function ($scope, $http) {
-//     $scope.hello = {name: "Boaz"};
-//     $scope.newName = "";
-//     $scope.sendPost = function() {
-//         var data = $.param({
-//             json: JSON.stringify({
-//                 name: $scope.newName
-//             })
-//         });
-//         $http.post("/echo/json/", data).success(function(data, status) {
-//             $scope.hello = data;
-//         })
-//     }                   
-// })
+
+myApp.controller('oneController', function ($scope, $http, $location) {
+    var id= location.hash
+    console.log(location)
+    
+    // var newId = id.replace('#/single/')
+    // console.log(newId)
+    // var realId= id.split('/').pop
+    console.log(id)
+        $http.get("http://localhost:3000/api/chirps/" +id)
+            .then(function (response) {
+                $scope.allChirps = response.data;
+                console.log(location.hash)
+            })
+    })
+myApp.controller('allGetController', function ($scope, $http, $location) {
+    $http.get("http://localhost:3000/api/chirps")
+        .then(function (response) {
+            $scope.allChirps = response.data
+        })
+    $scope.insertdata = function () {
+        console.log('clicked')
+        $http.post("http://localhost:3000/api/chirps", { 'user': $scope.user, 'message': $scope.message })
+            .success(function (data, status, headers, config) {
+                $scope.user = ''
+                $scope.message = ''
+                $http.get("http://localhost:3000/api/chirps")
+                    .then(function (response) {
+                        $scope.allChirps = response.data;
+
+                    })
+            });
+    }
+    $scope.goToSingle = function (id) {
+        $http.get('/api/chirps/')
+            .then(function () {
+                $location.path('/single/' + id)
+                id=($location.path('/single/' + id))
+            })
+
+    }
+    
+
+});
